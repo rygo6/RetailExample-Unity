ce866d6753d1c19e61d4280e19611c69a2121502
Ryan Goodrich <rygo6@me.com>
2016-05-18 22:41:58 -0700

    added start scene

---
+++ Assets/VisualizationFramework/Scripts/Components/StartSequence.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EC {
+public class StartSequence : MonoBehaviour {
+	void Start() {
+		SceneManager.LoadScene(1);
+	}
+}
+}
